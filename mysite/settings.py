ee3a2669b6a58e43c4305cb741c08b6700f88054
anan <850016457@qq.com>
2018-12-19 16:36:49 +0800

    使用pymysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,14 +81,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myblog_db',
+        'USER': 'ayp',
+        'PASSWORD': 'a18803100108',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
