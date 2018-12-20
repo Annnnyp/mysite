458f6f15b0032f5affd109e39762b90c76963643
anan <850016457@qq.com>
2018-12-20 16:47:43 +0800

    test

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -53,13 +53,13 @@ TEMPLATES = [
         ,
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_processors': [
+            'context_processors': {
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'user.context_processors.login_modal_form'
-            ],
+            },
         },
     },
 ]
